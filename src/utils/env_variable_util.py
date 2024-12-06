642c14e9e979fc4d27fdfc9ac105aaab7db11f30
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2024-12-06 17:12:51 +0200

    Created EnvVariableUtil

---
+++ src/utils/env_variable_util.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+
+class EnvVariableUtil:
+    @staticmethod
+    def get_env_variable(var_name):
+        try:
+            result = os.environ[var_name]
+            if EnvVariableUtil.validate(result):
+                return result
+            else:
+                raise KeyError(f"The variable {var_name} has invalid value {result}. Please update the variable.")
+        except KeyError:
+            raise KeyError(f"The variable {var_name} does not exist. Please go to .env file to set it.")
+
+    @staticmethod
+    def validate(result):
+        if result is None:
+            return False
+        if result == "":
+            return False
+        if result == '...':
+            return False
+        return True
