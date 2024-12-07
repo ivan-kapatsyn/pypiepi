78d3b465fa89e7d3ba6e130c5eb0be1ef74b525b
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2024-12-07 18:44:50 +0200

    Added PathUtil

---
+++ src/utils/path_util.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+
+from src.utils.env_variable_util import EnvVariableUtil
+
+
+class PathUtil:
+    @staticmethod
+    def get_project_path():
+        project_path = EnvVariableUtil.get_env_variable('PROJECT_PATH')
+        return project_path
+
+    @staticmethod
+    def get_template_path():
+        project_path = PathUtil.get_project_path()
+        template_path = EnvVariableUtil.get_env_variable('TEMPLATE_RELATIVE_PATH')
+        return Path(project_path, template_path)
+
+    @staticmethod
+    def get_static_path():
+        project_path = PathUtil.get_project_path()
+        static_path = EnvVariableUtil.get_env_variable('STATIC_RELATIVE_PATH')
+        return Path(project_path, static_path)
