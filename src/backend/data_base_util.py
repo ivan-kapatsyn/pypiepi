54179afa996b5986cb953930fb83d91db20c362e
ivan-kapatsyn <kapatsyn.ivan.ol@gmail.com>
2024-12-10 17:34:48 +0100

    Fixed issues

--- src/backend/data_base_util.py
+++ src/backend/data_base_util.py
@@ -3,8 +3,8 @@
 # TODO Remove this after the database is implemented
 # Dummy user database
 USERS = {
-    "user1": {"username": "user1", "password": "password123", "remember_me": True, "bio": "This is User 1's bio."},
-    "user2": {"username": "user2", "password": "securepass", "remember_me": True, "bio": "Welcome to User 2's personal bio page."}
+    "user1": {"user_id": "4eecc7ff58662618", "username": "user1", "password": "password123", "remember_me": True, "bio": "This is User 1's bio."},
+    "user2": {"user_id": "d5377bb4777aa34c", "username": "user2", "password": "securepass", "remember_me": True, "bio": "Welcome to User 2's personal bio page."}
 }
 
 def find_user_by_username(username: str):
@@ -15,3 +15,6 @@ def save_user(user_data):
 
 def users_with_remember_me():
     return {user: details for user, details in USERS.items() if details["remember_me"]}
+
+def get_user_ids():
+    return [user["user_id"] for user in USERS.values()]
