413306c2b87494b749003fbd03c5a3a145f40651
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2024-12-11 14:03:45 +0200

    Almost done with login

--- src/ui/routes.py
+++ src/ui/routes.py
@@ -10,6 +10,14 @@ class Routes(object):
     main_bp = Blueprint('main', __name__)
 
     @staticmethod
+    @main_bp.route('/process_suggestion', methods=['GET', 'POST'])
+    def process_suggestion():
+        suggestion = request.json.get('suggestion')  # Get the suggestion from the request
+        # Call your Python function with the suggestion
+        result = some_function_to_handle_click(suggestion)
+        return jsonify({'status': 'success', 'result': result})
+
+    @staticmethod
     @main_bp.route('/get_suggestions', methods=['GET', 'POST'])
     def get_suggestions():
         query = request.args.get('query', '')
@@ -17,7 +25,10 @@ class Routes(object):
             suggestions: List[User] = User.search_for_a_saved_users(query)
         else:
             suggestions = []
-        return jsonify([user.username for user in suggestions])
+        return jsonify([{
+            'username': user.username,
+            'user_id': user.user_id
+        }for user in suggestions])
 
     @staticmethod
     @main_bp.route('/login', methods=['GET', 'POST'])
@@ -36,9 +47,9 @@ class Routes(object):
         return render_template(login_page, form=form)
 
     @staticmethod
-    @main_bp.route('/personal_bio/<username>')
-    def personal_bio(username):
-        user = username
+    @main_bp.route('/personal_bio/<user_id>')
+    def personal_bio(user_id):
+        user = User.search_for_a_saved_users(user_id)
         if not user:
             flash('User not found', 'danger')
             return redirect(url_for('main.login'))
