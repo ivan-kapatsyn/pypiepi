4e2d7c91dc98fd289bae6a375648d3f3c5390767
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2024-12-13 16:42:12 +0200

    Added dynamic form components

--- src/ui/route_components/register_routes.py
+++ src/ui/route_components/register_routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+
+from flask import Blueprint, render_template, request
+
+from src.ui.forms.register_new_user import RegisterNewUser
 
 
 class RegisterRoutes:
@@ -7,5 +10,25 @@ class RegisterRoutes:
     @staticmethod
     @main_bp.route('/new_user', methods=['GET', 'POST'])
     def new_user():
-        login_page = r'register.html'
-        return render_template(login_page, message="You are being registered")
+        form = RegisterNewUser()
+        if form.validate_on_submit():
+            # Process form data
+            usertype = request.form.get('usertype')
+            additional_info = {}
+
+            if usertype == 'student':
+                additional_info['student_info'] = request.form.get('student_info')
+            elif usertype == 'tutor':
+                additional_info['tutor_info_1'] = request.form.get('tutor_info_1')
+                additional_info['tutor_info_2'] = request.form.get('tutor_info_2')
+            elif usertype == 'admin':
+                additional_info['admin_info_1'] = request.form.get('admin_info_1')
+                additional_info['admin_info_2'] = request.form.get('admin_info_2')
+                additional_info['admin_info_3'] = request.form.get('admin_info_3')
+
+            # Do something with the data
+            print(f"Usertype: {usertype}, Additional Info: {additional_info}")
+            return "Form Submitted Successfully!"
+
+        register_page = r'register.html'
+        return render_template(register_page, form=form)
