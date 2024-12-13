503ba68f4248d89318a11c43a3a255ab58c2f3f9
melisguel <melis.guel@web.de>
2024-12-13 15:23:57 +0100

    data_base_util is added to check if a specific table exists. It creates the tables with the rows by the information of the json-file. In _data.py it takes the information, creates a csv-file and add them to the table

--- src/utils/data_folder/studentinlesson_data.py
+++ src/utils/data_folder/studentinlesson_data.py
@@ -3,14 +3,12 @@ from src.utils.data_base_util import DataBaseUtil
 import psycopg2
 
 data = [
-    ["studentid", "userid", "firstname", "lastname", "course"],
-    [12,202345671, "arthur", "morgan", "economics"],
-    [123, 202345672, "john", "doe", "biology"],
-    [1234, 202345674, "john", "marston", "marketing"],
-    [12345, 202345675, "mary", "stuart", "physics"]
+    ["tutorid", "userid", "firstname", "lastname", "course"],
+    [5432, 202345675, "mary", "stuart", "physics"]
 ]
 
-csv_file = "../../../data_folder/student_table_data.csv"
+
+csv_file = "../../../data_folder/tutor_table_data.csv"
 
 try:
     with open(csv_file, 'w', newline='', encoding="utf-8") as csvfile:
@@ -33,7 +31,7 @@ class AddToUser(DataBaseUtil):
 
                 for row in reader:
                     print(f"Row: {row}")
-                    insert_query = f"INSERT INTO student (studentid, userid, firstname, lastname, course) VALUES ({row[0]}, '{row[1]}', '{row[2]}', '{row[3]}', '{row[4]}');"
+                    insert_query = f"INSERT INTO tutor (tutorid, userid, firstname, lastname, course) VALUES ({row[0]}, '{row[1]}', '{row[2]}', '{row[3]}', '{row[4]}');"
                     self.cursor.execute(insert_query)
 
             self.connection.commit()
@@ -47,6 +45,6 @@ class AddToUser(DataBaseUtil):
 if __name__ == "__main__":
     try:
         add_to_user = AddToUser()
-        add_to_user.import_csv("../../../data_folder/student_table_data.csv")
+        add_to_user.import_csv("../../../data_folder/tutor_table_data.csv")
     except Exception as error:
         print(f"Error: {error}")
