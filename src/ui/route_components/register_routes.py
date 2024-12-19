bba80672eadcf55b0b8663ed367d05ea2c16f66f
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2024-12-19 13:21:20 +0200

    Finished Register new User page for tutors

--- src/ui/route_components/register_routes.py
+++ src/ui/route_components/register_routes.py
@@ -19,15 +19,18 @@ class RegisterRoutes:
             if usertype == 'student':
                 additional_info['student_info'] = request.form.get('student_info')
             elif usertype == 'tutor':
-                additional_info['tutor_info_1'] = request.form.get('tutor_info_1')
-                additional_info['tutor_info_2'] = request.form.get('tutor_info_2')
+                additional_info['tutor_register_number'] = request.form.get('tutor_info_1')
+                additional_info['qualifications'] = request.form.getlist('tutor_info_2[]')
+                if len(additional_info['qualifications']) == 0 and additional_info['qualifications'][0] == '':
+                    additional_info['qualifications'] = []
             elif usertype == 'admin':
                 additional_info['admin_info_1'] = request.form.get('admin_info_1')
                 additional_info['admin_info_2'] = request.form.get('admin_info_2')
                 additional_info['admin_info_3'] = request.form.get('admin_info_3')
 
-            # Do something with the data
+            # Todo use Tutor.register_new_user()
             print(f"Usertype: {usertype}, Additional Info: {additional_info}")
+            # Todo Render a new page
             return "Form Submitted Successfully!"
 
         register_page = r'register.html'
