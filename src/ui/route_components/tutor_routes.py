0c35857d2c633ff2dc606fac41a0813d5adef55e
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2024-12-28 16:27:50 +0200

    Centered the logo

--- src/ui/route_components/tutor_routes.py
+++ src/ui/route_components/tutor_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 
+from src.backend.user import Tutor, User
+
 
 class TutorRoutes:
     main_bp = Blueprint('tutor', __name__, url_prefix='/tutor')
@@ -7,5 +9,7 @@ class TutorRoutes:
     @staticmethod
     @main_bp.route('/personal_bio/<user_id>', methods=['GET', 'POST'])
     def personal_bio(user_id: int):
+        # TODO make Tutor.get_user_by_id() work
+        tutor = User.get_user_by_id(str(user_id))
         personal_bio_page = r'tutor_personal_info.html'
-        return render_template(personal_bio_page)
+        return render_template(personal_bio_page, username=tutor.username)
