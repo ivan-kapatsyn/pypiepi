efb556d382085c9dfde6ee0179b1a8079038de3e
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2025-01-07 11:51:50 +0200

    Made basic bio work

--- src/ui/route_components/tutor_routes.py
+++ src/ui/route_components/tutor_routes.py
@@ -12,8 +12,8 @@ class TutorRoutes:
     @staticmethod
     @main_bp.route('/<user_id>/personal_bio', methods=['GET', 'POST'])
     def personal_bio(user_id: str):
-        # TODO make Tutor.get_user_by_id() work
-        tutor = User.get_user_by_id(user_id)
+        tutor = Tutor.get_user_by_id(user_id)
+        courses = tutor.active_courses
         # Todo get courses from tutor
         courses = [
             {"name": "Meeting", "day": "Monday", "time": "9AM", "url": f"/meeting"},
@@ -21,17 +21,21 @@ class TutorRoutes:
             {"name": "Standup", "day": "Wednesday", "time": "11AM", "url": "/standup"},
             # Add more events as needed
         ]
-
+        # Todo get data from dict(tutor)
+        data = [
+            {"name": "Name", "value": tutor.username}
+        ]
 
         personal_bio_page = r'tutor_personal_info.html'
         return render_template(personal_bio_page, username=tutor.username,
-                               user_id=user_id, events=courses,
+                               user_id=user_id,
+                               data = data,
+                               events=courses,
                                days=TutorRoutes.TIMETABLE_DAYS, time=TutorRoutes.TIMETABLE_TIME)
 
     @staticmethod
-    @main_bp.route('/<user_id>/toggle_remeber_me', methods=['GET', 'POST'])
-    def toggle_remeber_me(user_id: str):
+    @main_bp.route('/<user_id>/toggle_remember_me', methods=['GET', 'POST'])
+    def toggle_remember_me(user_id: str):
         user = User.get_user_by_id(user_id)
-        # TODO uncomment this code
-        # user.toggle_remeber_me()
+        user.toggle_saved_button()
         return Response(status=204)
