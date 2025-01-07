e8aaad46b8e696d07f967b37098d0fbc790c60da
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2025-01-07 12:45:36 +0200

    Refactored toggle_remember_me to /user

--- src/ui/route_components/tutor_routes.py
+++ src/ui/route_components/tutor_routes.py
@@ -1,11 +1,12 @@
-from flask import Blueprint, render_template, Response, request, jsonify
+from flask import Blueprint, render_template, request, jsonify
 
-from src.backend.user import Tutor, User
+from src.backend.user import Tutor
 
 
 class TutorRoutes:
     TIMETABLE_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-    TIMETABLE_TIME = [f"{hour}AM" if hour < 12 else (f"{hour - 12}PM" if hour > 12 else "12PM") for hour in range(9, 19)]
+    TIMETABLE_TIME = [f"{hour}AM" if hour < 12 else (f"{hour - 12}PM" if hour > 12 else "12PM") for hour in
+                      range(9, 19)]
 
     main_bp = Blueprint('tutor', __name__, url_prefix='/tutor')
 
@@ -30,17 +31,10 @@ class TutorRoutes:
 
         personal_bio_page = r'tutor_personal_info.html'
         return render_template(personal_bio_page, username=tutor.username,
-                               user_id=user_id,
-                               data = data,
+                               user_id=user_id, remember_me = tutor.remember_me,
+                               data=data,
                                events=courses,
                                days=TutorRoutes.TIMETABLE_DAYS, time=TutorRoutes.TIMETABLE_TIME)
-
-    @staticmethod
-    @main_bp.route('/<user_id>/toggle_remember_me', methods=['GET', 'POST'])
-    def toggle_remember_me(user_id: str):
-        user = User.get_user_by_id(user_id)
-        user.toggle_saved_button()
-        return Response(status=204)
 
     @staticmethod
     @main_bp.route('/<user_id>/update-data', methods=['GET', 'POST'])
