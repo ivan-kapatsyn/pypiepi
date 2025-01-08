aa2a2e48efe59f9a215d053761b637d28aee94e5
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2025-01-08 14:01:58 +0200

    Added validation to max_participants

--- src/ui/forms/create_new_course.py
+++ src/ui/forms/create_new_course.py
@@ -8,9 +8,10 @@ class CourseCreationForm(FlaskForm):
     course_name = StringField('Course Name', validators=[DataRequired()])
     qualification = SelectField('Qualification', validators=[DataRequired()], choices=[])
     room = StringField('Room', validators=[DataRequired()])
-    schedule = TextAreaField('Schedule (e.g., Mon-Fri 9AM-12PM)', validators=[DataRequired()])
-    max_participants = IntegerField('Max Number of Participants', validators=[
+    schedule = TextAreaField('Schedule (the format is "Mon 9AM". If you want to add more hours add them in a separate line)', validators=[DataRequired()])
+    max_participants = IntegerField('Total number of students', validators=[
         DataRequired(),
-        NumberRange(min=1, message="The number must be greater than 0.")
+        NumberRange(min=5, message="There should be more than 5 students."),
+        NumberRange(max=30, message="There should be less than 30 students in the class.")
     ])
     submit = SubmitField('Create')
