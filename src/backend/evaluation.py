f873853e6e0914adf7ed94e4df208a5f75b558b5
ivan-kapatsyn <kapatsyn.ivan.ol@gmail.com>
2025-01-25 14:24:18 +0100

    Just small reindent

--- src/backend/evaluation.py
+++ src/backend/evaluation.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 class Evaluation:
-  def __init__(self, evaluation_id: str, author, date: datetime, numeric_evaluation: int, feedback: str):
+    def __init__(self, evaluation_id: str, author, date: datetime, numeric_evaluation: int, feedback: str):
         self.evaluation_id = evaluation_id
         self.author = author
         self.date = date
