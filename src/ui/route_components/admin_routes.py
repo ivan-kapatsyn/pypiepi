721b1887e0ac5383c615315c3d4534d09b538ffa
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2025-02-02 20:24:24 +0200

    Added Admin routes

---
+++ src/ui/route_components/admin_routes.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, render_template
+
+from src.backend.user import User
+
+
+class AdminRoutes:
+    TIMETABLE_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    TIMETABLE_TIME = [f"{hour}AM" if hour < 12 else (f"{hour - 12}PM" if hour > 12 else "12PM") for hour in
+                      range(9, 19)]
+
+    main_bp = Blueprint('admin', __name__, url_prefix='/admin')
+
+    @staticmethod
+    @main_bp.route('/<user_id>/personal_bio', methods=['GET', 'POST'])
+    def personal_bio(user_id: str):
+        # TODO Replace it with Admin.get_user_by_id()
+        admin = User.get_user_by_id(user_id)
+        data = [
+            {"name": "Username", "value": admin.username},
+            {"name": "First Name", "value": admin.first_name},
+            {"name": "Last Name", "value": admin.last_name},
+            {"name": "Bio", "value": admin.bio},
+            {"name": "Role", "value": "Senior Administrator"}
+        ]
+        personal_bio_page = r'admin_personal_info.html'
+        return render_template(personal_bio_page, username=admin.username,
+                               user_id=user_id, remember_me=admin.remember_me,
+                               data=data)
