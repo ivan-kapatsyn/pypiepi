db6cb5d21f187a9e7688d3a7f105490a64604916
BirteMB <birtemaria.becker@stud.uni-goettingen.de>
2025-02-11 12:51:15 +0100

    added methods to admin_routes

--- src/ui/route_components/admin_routes.py
+++ src/ui/route_components/admin_routes.py
@@ -3,8 +3,12 @@ from numpy import number
 
 from src.backend.course import Course
 from src.backend.user import User
+
+from src.backend.plotting import generate_plot
+
 from src.ui.forms.room_overview import RoomOverview
 from src.ui.forms.token_generator import TokenGenerator
+
 
 
 class AdminRoutes:
@@ -32,12 +36,13 @@ class AdminRoutes:
         room_form.day.data = day
         room_form.hour.data = hour
         room_data = AdminRoutes.__get_room_data(day, hour)
+        plot_data = generate_plot()
 
         token_form = TokenGenerator()
         personal_bio_page = r'admin_personal_info.html'
         return render_template(personal_bio_page, username=admin.username,
                                user_id=user_id, remember_me=admin.remember_me,
-                               data=data, room_form=room_form,room_data=room_data, token_form=token_form)
+                               data=data, room_form=room_form,room_data=room_data, token_form=token_form, plot_data=plot_data,)
 
     @staticmethod
     @main_bp.route('<user_id>/save_token', methods=['POST'])
