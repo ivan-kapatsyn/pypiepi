8a39e19de8e34a26d75668376c7f0b8f21061bd4
andrii-demydenko <166001732+andrii-demydenko@users.noreply.github.com>
2025-02-14 21:02:58 +0200

    Added registering Students and Admins

--- src/ui/route_components/register_routes.py
+++ src/ui/route_components/register_routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 
+from src.backend.admin import Admin
 from src.backend.exceptions import DuplicationError, WrongTokenError
 from src.backend.qualification import Qualification
+from src.backend.student import Student
 from src.backend.tutor import Tutor
 from src.ui.forms.register_new_user import RegisterNewUser
 
@@ -31,8 +33,7 @@ class RegisterRoutes:
                     return render_template(register_page, form=form, error_message="Token has to be a number")
                 additional_info['study_program'] = request.form.get('student_info_2')
                 data = {**data, **additional_info}
-                # TODO add Student.register_new_user()
-                user_id = 1
+                user_id = Student.register_new_user(**data)
             elif user_type == 'tutor':
                 try:
                     additional_info['token'] = int(request.form.get('tutor_info_1'))
@@ -61,8 +62,7 @@ class RegisterRoutes:
                     return render_template(register_page, form=form, error_message="Token has to be a number")
                 additional_info['role'] = request.form.get('admin_info_2')
                 data = {**data, **additional_info}
-                # TODO add Admin.register_new_user()
-                user_id = 1
+                user_id = Admin.register_new_user(**data)
             return redirect(url_for(f'{user_type}.personal_bio', user_id=user_id))
 
 
