439cc468dd3925d498a366f890b96d894f8a26a0
ivan-kapatsyn <kapatsyn.ivan.ol@gmail.com>
2025-02-16 14:09:52 +0100

    Added Evaluation.get_by_id()

--- tests/backend/evaluation.py
+++ tests/backend/evaluation.py
@@ -3,7 +3,7 @@ from secrets import token_hex
 from src.backend.evaluation import Evaluation
 
 
-class TestRoom(unittest.TestCase):
+class TestEvaluation(unittest.TestCase):
 
     def test_get_evaluations_by_course_ids(self):
         course_ids = ["dc295cc4dd09d5b1", "39104442b2660b56"]
@@ -19,6 +19,20 @@ class TestRoom(unittest.TestCase):
         evaluations = Evaluation.get_evaluations_by_course_ids(course_ids)
 
         self.assertEqual(len(evaluations), 0)
+
+    def test_get_by_id(self):
+        evaluation_id = "46733e77c313bf45"
+        evaluation = Evaluation.get_by_id(evaluation_id)
+
+        self.assertIsNotNone(evaluation)
+        self.assertEqual(evaluation.evaluation_id, evaluation_id)
+        self.assertEqual(evaluation.course_id, "39104442b2660b56")
+
+    def test_get_by_id_invalid(self):
+        evaluation_id = "invalid_id"
+        evaluation = Evaluation.get_by_id(evaluation_id)
+
+        self.assertIsNone(evaluation)
 
 if __name__ == "__main__":
     unittest.main()
